0b51bd329aee6c9521c09e1399f7d5dc1d99d2da
Junihor Moran <junihor.moran@xselltechnologies.com>
2023-01-16 18:43:38 -0500

    Change: add color format to results

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import MinimumBribes from "./modules/bribes";
+import MinimumBribes from "./modules/arrays/bribes";
+import LeftRotation from "./modules/arrays/left_rotation";
 
 function home() {
-  MinimumBribes([1, 2, 5, 3, 7, 8, 6, 4]);
+  console.log("%cArrays Problems", "color:cyan; font-size:20px");
+  console.log("- Minimum of Bribes: ", MinimumBribes([1, 2, 5, 3, 7, 8, 6, 4]));
+  console.log("- Left Rotation: ", LeftRotation([1, 2, 3, 4, 5], 4));
 }
 
 home();
